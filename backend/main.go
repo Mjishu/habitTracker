e00213ea074059ff889243a484542d96dea8f927
mjishu <128941980+Mjishu@users.noreply.github.com>
2025-06-02 21:22:31 -0400

    setup go net

---
+++ backend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type apiConfig struct {
+	jwt_secret string;
+}
+
+func main() {
+	fmt.Println("Hello")
+
+	mux := http.NewServeMux()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("could not load .env, %s", err)
+	}
+
+	mux.HandleFunc("GET /users/{userID}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("called user for specific id")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("listening on port" + port)
+	err = http.ListenAndServe(":" + port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
