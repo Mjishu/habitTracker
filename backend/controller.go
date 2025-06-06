020fbea8d6da7d1cf9c22ddfdc30ff52a943e7f9
mjishu <128941980+Mjishu@users.noreply.github.com>
2025-06-05 22:12:00 -0400

    organized conntrollers

--- backend/controller.go
+++ backend/controller.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+//** --------------------------
+//**		User
+//** --------------------------
+
 func (conf apiConfig) UserById(w http.ResponseWriter, r *http.Request) {
 	userId := r.PathValue("userID")
 	fmt.Println("this is from inside of userbyid" + userId)
@@ -64,4 +68,56 @@ func (conf apiConfig) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "User deleted successfully")
+}
+
+//** --------------------------
+//**		Quests
+//** --------------------------
+
+func (conf apiConfig) AllQuests(w http.ResponseWriter, r *http.Request) {
+	sql := `
+		SELECT * FROM quests;
+	`
+
+	row := conf.db.QueryRow(sql)
+
+	var quests []Quest
+	err := row.Scan(&quests)
+	if err != nil {
+		http.Error(w, "Quest table not found", http.StatusNotFound)
+		return
+	}
+}
+
+func (conf apiConfig) QuestById(w http.ResponseWriter, r *http.Request) {
+	questId := r.PathValue("QuestID")
+	sql := `
+		SELECT * FROM quests WHERE id = ?
+	`
+
+	row := conf.db.QueryRow(sql, questId)
+
+	var questInfo Quest 
+
+	err := row.Scan(&questInfo)
+	if err != nil {
+		http.Error(w, "Quest not found", http.StatusNotFound)
+		return
+	}
+}
+
+//** --------------------------
+//**		Rewards
+//** --------------------------
+
+func (config apiConfig) GetAllRewards(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (config apiConfig) GetAllQuestRewards(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (config apiConfig)  GetRewardById(w http.ResponseWriter, r *http.Request) {
+
 }
